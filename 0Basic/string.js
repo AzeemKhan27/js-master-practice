c40de94a682f5300177fd7268c6555deec2a42c8
AzeemKhan27 <121457811+AzeemKhan27@users.noreply.github.com>
2023-11-28 18:28:26 +0000

    adding nums and maths code

---
+++ 0Basic/string.js
@@ -0,0 +1,17 @@
+const gameName = new String('hitesh-hc-com')
+
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
+
+//console.log(gameName.substring(0,4));  //newString
+
+console.log(gameName.slice(-8,4));     //anotherString
+
+const url = "https://hitesh.com/hitesh%20/choudhary";
+console.log(url.replace('%20','-'))
+
+console.log(url.includes('hitesh')) // is this value available over there.
+
+console.log(gameName.split('-'))
+
+
