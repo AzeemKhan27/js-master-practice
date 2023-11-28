be6fc5640432d848747997b25914eadfefad0606
AzeemKhan27 <121457811+AzeemKhan27@users.noreply.github.com>
2023-11-28 13:31:03 +0000

    adding datatype summary

---
+++ 0Basic/datatype-summary.js
@@ -0,0 +1,25 @@
+
+
+//Primitive 
+
+// String , Number , Boolean , null , undefined , Symbol , BigInt
+
+//Reference (Non-primitive)
+
+//Array , Object , Function
+
+// ----------Return type of variables in JavaScript---------
+
+// 1) Primitive Datatypes
+//        Number => number
+//        String  => string
+//        Boolean  => boolean
+//        null  => object
+//        undefined  =>  undefined
+//        Symbol  =>  symbol
+//        BigInt  =>  bigint
+
+// 2) Non-primitive Datatypes
+//        Arrays  =>  object
+//        Function  =>  function
+//        Object  =>  object
--- 0Basic/datatype.js
+++ 0Basic/datatype.js
@@ -11,3 +11,16 @@
 
 console.log(typeof undefined) //undefined
 console.log(typeof null) //object
+
+//When We use Number() method:-
+
+//"33" => 33
+//"33abc" => NaN but Type will number
+//true => 1; false => 0
+
+//When We use Boolean Type:-
+
+// 1 => true ; 0 => false
+// "" => false
+// "hitesh sir" => true
+
