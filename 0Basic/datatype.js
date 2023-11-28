008db25a0e1c6d972b4d62acaabd6abb10e31a94
AzeemKhan27 <121457811+AzeemKhan27@users.noreply.github.com>
2023-11-28 11:57:35 +0000

    dataType in JS

---
+++ 0Basic/datatype.js
@@ -0,0 +1,13 @@
+
+
+// number => 2 to the power 53
+// bigint => used for big values
+// string => ""
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+//object
+
+console.log(typeof undefined) //undefined
+console.log(typeof null) //object
