6cab36a2cc2b1610feefa98fe465ee43c035a7a3
AzeemKhan27 <121457811+AzeemKhan27@users.noreply.github.com>
2023-11-29 10:01:01 +0000

    normal function

---
+++ 03Basic/01function.js
@@ -0,0 +1,13 @@
+
+function addTwoNumber(num1, num2){
+    return num1 + num2
+}
+
+const result = addTwoNumber(2,3)
+// console.log("Result", result)
+
+function loginUserMessage(username){
+    return `${username} is LoggedIn`
+}
+
+console.log(loginUserMessage("Azeem Khan"));
