8852051c050ff149c48fc8ccad86db2e7ffb8a17
AzeemKhan27 <121457811+AzeemKhan27@users.noreply.github.com>
2024-02-04 06:21:52 +0000

    switch case in javascript

---
+++ 03_control-flow/switch.js
@@ -0,0 +1,42 @@
+// const month = 3
+
+// switch(month){
+//     case 1:
+//         console.log("January");
+//         break;
+//     case 2:
+//         console.log("Feb");
+//         break;
+//     case 3:
+//         console.log("March");
+//         break;
+//     case 4:
+//         console.log("April");
+//         break;
+
+//     default:
+//         console.log("default case match")
+//         break;    
+// }
+
+
+const month = "march"
+
+switch(month){
+    case "Jan":
+        console.log("January");
+        break;
+    case "Feb":
+        console.log("Feb");
+        break;
+    case "March":
+        console.log("March");
+        break;
+    case "April":
+        console.log("April");
+        break;
+
+    default:
+        console.log("default case match")
+        break;    
+}
