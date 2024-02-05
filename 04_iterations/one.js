c46beb96c90befe8fa04417ab66a196352b3d816
AzeemKhan27 <121457811+AzeemKhan27@users.noreply.github.com>
2024-02-05 06:50:02 +0000

    iteration with "for-loop"

---
+++ 04_iterations/one.js
@@ -0,0 +1,14 @@
+// for(let i=1; i<=10; i++){
+//     for(let j=1; j<=10; j++){
+//         console.log(`${i} x ${j} = ${i*j}`)
+//     }
+//     console.log('\n')
+// }
+
+const array = ['flash','bar','flat'];
+console.log(array.length);
+
+for(let i = 0; i < array.length; i++){
+    const ele = array[i];
+    console.log(ele);
+}
