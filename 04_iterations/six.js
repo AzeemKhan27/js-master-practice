91c8fd40ab3203803153fb28a6a68d36b936baf9
AzeemKhan27 <121457811+AzeemKhan27@users.noreply.github.com>
2024-02-06 05:39:30 +0000

    iteration in array of object with filter() function in JS

---
+++ 04_iterations/six.js
@@ -0,0 +1,46 @@
+// ++++++++++++++++++ forEach() +++++++++++++++++++++
+
+// const coding = ["js","python","ruby","c++","java"]
+
+// const values = coding.forEach((item)=>{
+//     console.log(item);
+//     return item;                          //if we want to return or not, forEach can't return any single array item.
+// })
+// console.log(values);                      //it means, we can iterate array with forEach but this method can't return.              
+
+// ++++++++++++++++++ filter() +++++++++++++++++++++
+
+const myNums = [1,2,3,4,5,6,7,8,9,10];
+
+// const newNums = myNums.filter((num)=>{
+//     return num > 5
+// })
+
+// ++++++++++++++++++ forEach() +++++++++++++++++++++
+
+// const newNums = [];
+// myNums.forEach((numbers)=>{
+//     if(numbers <= 5) {
+//         newNums.push(numbers)
+//     }
+// })
+// console.log(newNums);
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+const books = [
+    { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+    { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+    { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+    { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+    { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+    { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+    { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+    { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
+    { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
+  ];
+
+  const userData = books.filter((bk) => bk.genre === 'History');
+  const userBooksPublish = books.filter((bk) => bk.publish > 2000);
+//   console.log(userData);
+  console.log(userBooksPublish);
